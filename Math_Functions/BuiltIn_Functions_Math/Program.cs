054f1dc6b9e7533f12381e61a1472b60d5331c94
mehmet asım sezer <96918156+MehmetAsimSEZER@users.noreply.github.com>
2022-08-04 14:51:59 +0300

    Update Program.cs

--- Math_Functions/BuiltIn_Functions_Math/Program.cs
+++ Math_Functions/BuiltIn_Functions_Math/Program.cs
@@ -18,5 +18,36 @@ namespace BuiltIn_Functions_Math
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+        
+        //Console.WriteLine(DateTime.Now);
+        //Console.WriteLine(DateTime.Now.Date);
+        //Console.WriteLine(DateTime.Now.Day);
+        //Console.WriteLine(DateTime.Now.Month);
+        //Console.WriteLine(DateTime.Now.Year);
+        //Console.WriteLine(DateTime.Now.Hour);
+        //Console.WriteLine(DateTime.Now.Minute);
+        //Console.WriteLine(DateTime.Now.Second);
+        //Console.WriteLine(DateTime.Now.DayOfWeek);
+        //Console.WriteLine(DateTime.Now.DayOfYear);
+        
+        //Console.WriteLine(DateTime.Now.ToLongDateString);
+        //Console.WriteLine(DateTime.Now.ToShortDateString);
+        //Console.WriteLine(DateTime.Now.ToLongTimeString);
+        //Console.WriteLine(DateTime.Now.ToShortTimeString);
+        
+        //Console.WriteLine(DateTime.Now.AddDays(2));
+        //Console.WriteLine(DateTime.Now.AddHours(3));
+        //Console.WriteLine(DateTime.Now.AddSecond(4));
+        //Console.WriteLine(DateTime.Now.AddMonths(5));
+        //Console.WriteLine(DateTime.Now.AddYears(6));
+        
+        //Console.WriteLine(DateTime.Now.ToString("dd"));//24
+        //Console.WriteLine(DateTime.Now.ToString("dddd"));//Saturday
+        
+        //Console.WriteLine(DateTime.Now.ToString("MM"));//04
+        //Console.WriteLine(DateTime.Now.ToString("MMMM"));//April
+        
+        //Console.WriteLine(DateTime.Now.ToString("yy"));//21
+        //Console.WriteLine(DateTime.Now.ToString("yyyy"));//2021
     }
 }
